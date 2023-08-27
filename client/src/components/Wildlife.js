8667580abec4b585c11c497c8ebc7acb685fd9f2
BIBHAB22 <bibhabindia@gmail.com>
2023-08-28 01:27:40 +0530

    update

--- client/src/components/Wildlife.js
+++ client/src/components/Wildlife.js
@@ -7,18 +7,6 @@ import './Wildlife.css'; // Make sure to create a CSS file for styling
 const tourismSpots = [
   {
     id: 1,
-    name: 'Jim Corbett National Park',
-    description: "Where tigers roam free: Jim Corbett's majestic domain",
-    imageUrl: 'https://i.pinimg.com/564x/67/90/ed/6790ed4cc45790d7be9ae88bcf1619a2.jpg',
-  },
-  {
-    id: 2,
-    name: 'Kaziranga National Park',
-    description: 'Kaziranga: Where rhinos roam and nature reigns',
-    imageUrl: 'https://i.pinimg.com/564x/20/9c/22/209c22432420096b150c584128b1704f.jpg',
-  },
-  {
-    id: 1,
     name: 'Sundarbans National Park',
     description: "Tigers of the tide: Sundarbans' majestic guardians.",
     imageUrl: 'https://i.pinimg.com/564x/40/5a/ae/405aae5c581dfef088ac771b22e6bf4c.jpg',
@@ -41,7 +29,18 @@ const tourismSpots = [
     description: "Gorumara: Where the forest's heart beats to the rhythm of the wild",
     imageUrl: 'https://i.pinimg.com/564x/d9/ea/f4/d9eaf41d63fe1e72f4ebe53a756c2c97.jpg',
   },
-
+  {
+    id: 1,
+    name: 'Jim Corbett National Park',
+    description: "Where tigers roam free: Jim Corbett's majestic domain",
+    imageUrl: 'https://i.pinimg.com/564x/67/90/ed/6790ed4cc45790d7be9ae88bcf1619a2.jpg',
+  },
+  {
+    id: 2,
+    name: 'Kaziranga National Park',
+    description: 'Kaziranga: Where rhinos roam and nature reigns',
+    imageUrl: 'https://i.pinimg.com/564x/20/9c/22/209c22432420096b150c584128b1704f.jpg',
+  },
 
 ];
 
